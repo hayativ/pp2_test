8f69ac880c10c757cb91f819da6c69d38ada650c
Eva <davydova.04.eva@mail.ru>
2023-11-09 22:43:36 +0600

    Delete lab7_10.py

--- lab7_10.py
+++
@@ -1,8 +0,0 @@
-import re
-def my_func(string):
-    result = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', string)
-    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', result).lower()
-with open('row.txt', 'r') as fp:
-       text_list = fp.readlines()
-for i in text_list:
-       print(my_func(i))
