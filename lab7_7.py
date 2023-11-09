13581757e4eb490a5ca8f873bd8c2db21b0adfcc
Eva <davydova.04.eva@mail.ru>
2023-11-09 22:45:51 +0600

    Delete lab7_7.py

--- lab7_7.py
+++
@@ -1,7 +0,0 @@
-import re
-def my_func(string):
-    return ''.join(x.capitalize() or '_' for x in string.split('_'))
-with open('row.txt', 'r') as fp:
-       text_list = fp.readlines()
-for i in text_list:
-       print(my_func(i))
