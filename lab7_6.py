1a5387184814123273613f078411d4c50c211375
Eva <davydova.04.eva@mail.ru>
2023-11-09 22:45:29 +0600

    Delete lab7_6.py

--- lab7_6.py
+++
@@ -1,7 +0,0 @@
-import re
-def my_func(string):
-  return re.sub("[ ,.]", ":", string)
-with open('row.txt', 'r') as fp:
-       text_list = fp.readlines()
-for i in text_list:
-       print(my_func(i))
